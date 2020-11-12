5be966473d7acbd852cd92d65eeeeff17c7298f1
caco-jr <jr.carlosad@gmail.com>
2020-11-11 21:46:52 -0300

    refactor: Removendo componente desnecessário

--- src/components/Countdown/index.tsx
+++
@@ -1,5 +0,0 @@
-import { CountdownWrapper } from './style';
-
-const Countdown = (): JSX.Element => <CountdownWrapper>Hello</CountdownWrapper>;
-
-export default Countdown;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 import { HomeTitle } from './style';
 import Layout from '@components/Layout';
-import Countdown from '@components/Countdown';
 
 export const HomePage = (): JSX.Element => (
   <Layout>
     <section className="container">
-      <HomeTitle>
-        Falta <Countdown /> para 2020 acabar
-      </HomeTitle>
+      <HomeTitle>Hello World!</HomeTitle>
     </section>
   </Layout>
 );
