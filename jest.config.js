fbe9d2ee375291ad683ed30b2910a2e73d12ba93
Carlos Costa <jr.carlosad@gmail.com>
2022-09-03 21:46:32 -0300

    feat: Configurando testes

--- jest.config.js
+++ jest.config.js
@@ -13,9 +13,6 @@ const customJestConfig = {
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
-  },
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
